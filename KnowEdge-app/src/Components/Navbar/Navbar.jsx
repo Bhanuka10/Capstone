ae88efb9e076f4fa2a560d54dbb6d7bc95534644
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-03-26 11:08:46 +0530

    create html of nav bar

--- KnowEdge-app/src/Components/Navbar/Navbar.jsx
+++ KnowEdge-app/src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 import './Navbar.css'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className='first'>
+        <div className='logo'>
+            <img src="DALL·E 2025-03-07 23.02.25 - A modern, sleek logo design combining the letters K and E in an elegant and creative way. The design should focus on a clean, minimalistic aesthetic w.png" alt="" />
+        </div>
+        <div className='search'>
+
+        </div>
+        <div className='menu'>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        </div>
+        <div  className='second'>
+            <div className='login-btn'>
+                <button>Sign in</button>
+            </div>
+            <div className='user-logo'>
+                <img src="av3cbfdc7ee86dab9a41d.png" alt="" />
+            </div>
+        </div>
+    </div>
   )
 }
 
