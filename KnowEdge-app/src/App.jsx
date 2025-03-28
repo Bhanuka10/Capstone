cc5a07db414639e51f1afef699130d3fd9b8f78b
maria <mariyetavishanthi11@gmail.com>
2025-03-29 01:43:22 +0530

    Updated MyProfile Header

--- KnowEdge-app/src/App.jsx
+++ KnowEdge-app/src/App.jsx
@@ -3,11 +3,13 @@ import Navbar from './Components/Navbar/Navbar'
 import Home from './Pages/Home/Home'
 import MyProfile from './Pages/Home/MyProfile/MyProfile'
 import { Route, Routes } from 'react-router-dom'
+import MyProfileHeader from './Components/MyProfileHeader/MyProfileHeader'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
+      <MyProfileHeader/>
       <Routes>
         <Route path="/" element={<Home />} /> 
         <Route path='/' element={<MyProfile/>}/>
--- KnowEdge-app/src/Components/MyProfileHeader/MyProfileHeader.jsx
+++ KnowEdge-app/src/Components/MyProfileHeader/MyProfileHeader.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import './MyProfileHeader.css'
 
-function MyProfileHeader() {
+const MyProfileHeader=() => {
   return (
     <div>
-      
+      <div className='profile-container'>
+        {/*knowedge box*/}
+        <div className='knowedge-box'>
+            <h2 className='title'>KnowEdge</h2>
+            <p className='subtitle'>Sharpen your knowledge with AI-powered guidance for a smarter learning journey</p>
+            <button className='chatbot-button'>Use Chatbot</button>
+        </div>
+
+        {/*user profile box*/}
+        <div className='profile-box'>
+            <img src='C:\Users\ADMIN\Documents\GitHub\Capstone\KnowEdge-app\src\Images\Maria.jpg' alt='User' className='profile-image'></img>
+            <h3 className='username'>Hello Maria!</h3>
+            <p className='user-role'>Bla Bla</p>
+            
+        </div>
+      </div>
     </div>
   )
 }
