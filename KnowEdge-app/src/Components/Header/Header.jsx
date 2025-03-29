133cec529891d1150f255bf1585e2b476197ad71
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-03-29 17:59:31 +0530

    update java script part

--- KnowEdge-app/src/Components/Header/Header.jsx
+++ KnowEdge-app/src/Components/Header/Header.jsx
@@ -4,9 +4,26 @@ const Header = () => {
   return (
     <div className='header'>
         <div>
-            
+            <div>
+           <h1>KnowEdge</h1>
+            <p>Sharpen Your Knowledge with AI-Powered Guidance for a Smarter Learning Journey</p>
         </div>
-        
+        <div>
+            <button>view courses</button>
+            <button>Use ChatBot</button>
+        </div> 
+
+        </div>
+        <div>
+            <div>
+            <img src="pngtree-3d-bot-ai-powered-marketing-and-notification-tools-png-image_9187783.png" alt="" />
+            <img src="pngtree-round-gradient-black-shadow-png-image_4317601.png" alt="" />
+            </div>
+            <div>
+            <img src="young-woman-sitting-floor-with-laptop-white.png" alt="" />
+            <img src="pngtree-round-gradient-black-shadow-png-image_4317601.png" alt="" />
+            </div>
+        </div>
     </div>
   )
 }
