9c45684614ec275a37032d1379453dc7b2b7b84a
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-03-30 19:48:47 +0530

    create header left side

--- KnowEdge-app/src/Components/Header/Header.jsx
+++ KnowEdge-app/src/Components/Header/Header.jsx
@@ -3,25 +3,25 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-        <div>
-            <div>
+        <div className='header-left'>
+            <div className='header-left-top'>
            <h1>KnowEdge</h1>
             <p>Sharpen Your Knowledge with AI-Powered Guidance for a Smarter Learning Journey</p>
         </div>
-        <div>
+        <div className='header-left-bottom'>
             <button>view courses</button>
             <button>Use ChatBot</button>
         </div> 
 
-        </div>
-        <div>
-            <div>
-            <img src="pngtree-3d-bot-ai-powered-marketing-and-notification-tools-png-image_9187783.png" alt="" />
-            <img src="pngtree-round-gradient-black-shadow-png-image_4317601.png" alt="" />
+        </div >
+        <div className='header-right'>
+            <div className='right-one'>
+            <img className='robo' src="pngtree-3d-bot-ai-powered-marketing-and-notification-tools-png-image_9187783.png" alt="" />
+            <img className='robo-shadow' src="pngtree-round-gradient-black-shadow-png-image_4317601.png" alt="" />
             </div>
-            <div>
-            <img src="young-woman-sitting-floor-with-laptop-white.png" alt="" />
-            <img src="pngtree-round-gradient-black-shadow-png-image_4317601.png" alt="" />
+            <div className='right-two'>
+            <img className='lady' src="young-woman-sitting-floor-with-laptop-white.png" alt="" />
+            <img className='lady-shadow' src="pngtree-round-gradient-black-shadow-png-image_4317601.png" alt="" />
             </div>
         </div>
     </div>
