8c1eed770cc3dc9d1ac29b7f014778dddab29c62
maria <mariyetavishanthi11@gmail.com>
2025-04-06 13:01:54 +0530

    Updated after cloning again

--- KnowEdge-app/src/Components/MyProfileHeader/MyProfileHeader.jsx
+++ KnowEdge-app/src/Components/MyProfileHeader/MyProfileHeader.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import './MyProfileHeader.css'
-<<<<<<< Updated upstream
 import userimage from '../../Images/Maria.jpg'
-=======
-/*import userimage from '../../Images/Maria.jpg'*/
->>>>>>> Stashed changes
 
 const MyProfileHeader=() => {
   return (
     <div>
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
       <div className='profile-container'>
         {/*knowedge box*/}
         <div className='knowedge-box'>
@@ -23,11 +15,7 @@ const MyProfileHeader=() => {
 
         {/*user profile box*/}
         <div className='profile-box'>
-<<<<<<< Updated upstream
             <img src={userimage} alt='User' className='profile-image'></img>
-=======
-            <img src='C:\Users\ADMIN\Documents\GitHub\Capstone\KnowEdge-app\src\Images\Maria.jpg' alt='User' className='profile-image'></img>
->>>>>>> Stashed changes
             <h3 className='username'>Hello Maria!</h3>
             <p className='user-role'>Bla Bla</p>
 
