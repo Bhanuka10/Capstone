6397bc3bd5b6d07c08bedc3f8c808a00e5635da2
maria <mariyetavishanthi11@gmail.com>
2025-04-09 12:13:23 +0530

    Created Chat Bot Banner

---
+++ KnowEdge-app/src/Components/BannersMyProfile/ChatBotBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ChatBotBanner.css'
+
+const ChatBotBanner = () => {
+  return (
+    <div className='chatbot-banner'>
+      <h2>Use Chatbot</h2>
+    </div>
+  )
+}
+
+export default ChatBotBanner
