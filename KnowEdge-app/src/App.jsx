7ab40769b1100122ffcb2468f614ff57c90499ca
maria <mariyetavishanthi11@gmail.com>
2025-04-09 19:15:35 +0530

    Updated

--- KnowEdge-app/src/App.jsx
+++ KnowEdge-app/src/App.jsx
@@ -4,12 +4,14 @@ import Home from './Pages/Home/Home';
 import { Route, Routes } from 'react-router-dom';
 import Myprofile from './Pages/Myprofile/Myprofile';
 import MyProfileHeader from './Components/MyProfileHeader/MyProfileHeader';
+import ChatBotBanner from './Components/BannersMyProfile/ChatBotBanner';
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
       <MyProfileHeader />
+      <ChatBotBanner/>
       <Routes>
         <Route path="/" element={<Home />} /> 
         <Route path="/myprofile" element={<Myprofile />} />
