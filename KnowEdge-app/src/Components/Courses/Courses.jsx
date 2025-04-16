29883ff66686060f1fb4b51b873af5a87fbed887
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-04-16 23:13:20 +0530

    new update

--- KnowEdge-app/src/Components/Courses/Courses.jsx
+++ KnowEdge-app/src/Components/Courses/Courses.jsx
@@ -1,26 +1,13 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './Courses.css'
 
 const Courses = () => {
   return (
     
-    <div className="Couses">
+    <div >
         
-        <ul class="nav nav-tabs">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-  </li>
-</ul>
+        
     </div>
   )
 }
