705035e114b7fc49cc6293ab52ac48e3be031990
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-05-03 20:26:01 +0530

    Updated one

--- KnowEdge-app/src/main.jsx
+++ KnowEdge-app/src/main.jsx
@@ -1,10 +1,24 @@
+// main.jsx or index.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App';
+import Myprofile from './Pages/Myprofile/Myprofile';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/contact",
+    element: <Myprofile />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
