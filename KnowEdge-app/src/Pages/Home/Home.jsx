9b10b21063039ad4cbabe0b0cc5b65970325e022
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-05-03 23:27:20 +0530

    apply footer in home jsx

--- KnowEdge-app/src/Pages/Home/Home.jsx
+++ KnowEdge-app/src/Pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import './Home.css'
 import Header from '../../Components/Header/Header'
 import About from '../../Components/About/About'
 import Courses from '../../Components/Courses/Courses'
+import Footer from '../../Components/Footer/Footer'
 const Home = () => {
   return (
     <div>
         <Header />
         <About />
         <Courses />
+        <Footer />
     </div>
   )
 }
