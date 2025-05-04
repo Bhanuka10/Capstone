63da9d35ecd27c0084f772b81f014cf93cd0be17
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-05-05 01:20:20 +0530

    end of today

--- KnowEdge-app/src/Components/Comment/Comment.jsx
+++ KnowEdge-app/src/Components/Comment/Comment.jsx
@@ -4,8 +4,17 @@ const Comment = () => {
   return (
     <div className='comment'>
         <div className='banner'><h1>Feedback</h1></div>
+        <div className='comment-container'>
+            <div className='first-column'>
+                <div className='comment-first-box'>
+                    <div>
+                        
+                    </div>
 
+                </div>
+            </div>
 
+        </div>
     </div>
   )
 }
