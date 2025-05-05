98558b5e864db4e1198e1869830f4ce964da90ca
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-05-05 21:29:08 +0530

    complete the comment box
    home page front end is over

--- KnowEdge-app/src/Components/Comment/Comment.jsx
+++ KnowEdge-app/src/Components/Comment/Comment.jsx
@@ -18,6 +18,44 @@ const Comment = () => {
 
 
                 </div>
+                <div className='comment-first-box'>
+                    <div className='id'>
+                        <img src="download.jpeg" alt="" />
+                        <h2>Sophia Williams</h2>
+                    </div>
+                    <div className='comment-text'>
+                        <p>"I appreciate how well-organized and informative this website is. The content is valuable, and the user experience is smooth. Keep it up!"</p>
+                    </div>
+                    
+
+
+                </div>
+            </div>
+            <div className='first-column'>
+                <div className='comment-first-box'>
+                    <div className='id'>
+                        <img src="handsome-man-pointing-lateral.jpg" alt="" />
+                        <h2>Daniel Smith</h2>
+                    </div>
+                    <div className='comment-text'>
+                        <p>"I love the clean layout and the variety of resources available. The site loads quickly, and everything is easy to find. Keep up the fantastic work!"</p>
+                    </div>
+                    
+
+
+                </div>
+                <div className='comment-first-box'>
+                    <div className='id'>
+                        <img src="young-smiling-young-woman-showing-copy-space.jpg" alt="" />
+                        <h2>Emily Granger</h2>
+                    </div>
+                    <div className='comment-text'>
+                        <p>"Fantastic website! The layout is clean and intuitive, making it easy to find information quickly. The content is clear, engaging, and highly informative. Well done!"</p>
+                    </div>
+                    
+
+
+                </div>
             </div>
 
         </div>
