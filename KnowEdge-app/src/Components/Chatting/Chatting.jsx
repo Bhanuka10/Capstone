2f89882614ebb6265e2020fd1b01c9b51a73c72f
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-05-10 00:14:42 +0530

    create expand of the side bar

--- KnowEdge-app/src/Components/Chatting/Chatting.jsx
+++ KnowEdge-app/src/Components/Chatting/Chatting.jsx
@@ -1,42 +1,44 @@
 import React from 'react'
 import './Chatting.css'
-import {assets} from '../../assets/assets'
+import { assets } from '../../assets/assets'
 
 const Chatting = () => {
-  return (
-    <div className='sidebar'>
-        <div className='top'>
-            <img className='menu' src={assets.menu_icon} alt="" />
-            <div className='new-chat'>
-                <img src={assets.plus_icon} alt="" />
-                <p>New Chat</p>
-            </div>
-        
-        <div className="recent">
-            <p className="recent-title">Recent</p>
-            <div className="recent-entry">
-                <img src={assets.message_icon} alt="" />
-                <p>What is react ....</p>
-            </div>
+    const [extended, setExtended] = React.useState(false);
+    return (
+        <div className={`sidebar ${extended ? 'expanded' : 'collapsed'}`}>
+    <div className='top'>
+        <img className='menu' src={assets.menu_icon} alt="" onClick={() => setExtended(!extended)} />
+        <div className='new-chat'>
+            <img src={assets.plus_icon} alt="" />
+            {extended && <p>New Chat</p>}
         </div>
+        {extended &&
+            <div className="recent">
+                <p className="recent-title">Recent</p>
+                <div className="recent-entry">
+                    <img src={assets.message_icon} alt="" />
+                    <p>What is react ....</p>
+                </div>
+            </div>
+        }
+    </div>
+    <div className='bottom'>
+        <div className="bottom-item">
+            <img src={assets.question_icon} alt="" />
+            {extended && <p>Help</p>}
         </div>
-        <div className='bottom'>
-            <div className="bottom-item ">
-                <img src={assets.question_icon} alt="" />
-                <p>Help</p>
-            </div>
-            <div className="bottom-item ">
-                <img src={assets.history_icon} alt="" />
-                <p>Activity</p>
-            </div>
-            <div className="bottom-item ">
-                <img src={assets.setting_icon} alt="" />
-                <p>Setings</p>
-            </div>
-            
+        <div className="bottom-item">
+            <img src={assets.history_icon} alt="" />
+            {extended && <p>Activity</p>}
+        </div>
+        <div className="bottom-item">
+            <img src={assets.setting_icon} alt="" />
+            {extended && <p>Settings</p>}
         </div>
     </div>
-  )
+</div>
+
+    )
 }
 
 export default Chatting
