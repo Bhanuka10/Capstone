c9cef8b475fcbb855d75378693c3651790a93b1c
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-05-10 22:56:00 +0530

    create chat bot

--- KnowEdge-app/src/Components/ChatBot/ChatBot.jsx
+++ KnowEdge-app/src/Components/ChatBot/ChatBot.jsx
@@ -1,26 +1,51 @@
 import React from 'react'
 import './ChatBot.css'
+import { assets } from '../../assets/assets'
 
 const ChatBot=() => {
   return (
-    <div className='chatbot-container'>
-      <h1 className='chatbot-heading'>Hello There</h1>
-      <h2 className='subheading'>How can I help you today?</h2>
+    <div className="main">
+      <div className='nav' >
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
 
-<div className='prompt-grid'>
-    <div className='prompt-box'>Can you create step-by-step roadmap for becoming a full stack developer</div>
-    <div className='prompt-box'>How should I structure my daily study schedule to master AI and machine learing in 6 months?</div>
-<div className='prompt-box'>What are the essential topics I need to learn to become a cybersecurity expert?</div>
-</div>
+      </div>
+      <div className='main-container'>
+        <div className="greet">
+          <p><span>Hellow,there</span></p>
+          <p>How can i Help you today</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Can you create a step-by-step roadmap for becoming a full-stack developer?</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>How should I structure my daily study schedule to master AI and machine learning in 6 months?</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>What are the essential topics I need to learn to become a cybersecurity expert?</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
 
-<div className='input-section'>
-  <input type='text' placeholder='Enter a prompt here' className='prompt-input'/>
-  <button className='generate-button'>Generate</button>
-  
-</div>
+        </div>
+        <div className="main-botom">
+          <div className="serch-box">
+            <input type="text" placeholder='Enter a prompt here' />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottom-info">
+            Chat bot is a large language model trained by Gemini 
+          </p>
+        </div>
 
+      </div>
     </div>
-
     
   )
 }
