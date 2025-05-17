3f6de38ad16b67bb2e2deb6ee4fecffcadc08ee7
harishihan-c <harishihan@outlook.com>
2025-05-17 21:53:30 +0530

    Add dash and sign in to context jsx for routing

--- KnowEdge-app/src/Context/Context.jsx
+++ KnowEdge-app/src/Context/Context.jsx
@@ -2,9 +2,25 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "../Pages/Home/Home"; // Assuming Home component exists
 import Myprofile from "../Pages/Myprofile/Myprofile"; // Assuming Myprofile component exists
-import ChatBot from "../Components/ChatBot/ChatBot"; // ChatBot component
+import ChatBot from "../Components/ChatBot/ChatBot";
+import Dashboard from "@/Components/AdminPannel/dashboard.jsx";
+import AddCourse from "@/Components/AdminAddCourses/AddCourse.jsx";
+import User from "@/Components/AdminUser/User.jsx";
+import Feedback from "@/Components/AdminFeedBack/Feedback.jsx";
+import WatchLater from "@/Components/WatchLater/WatchLater.jsx";
+import Register from "@/Pages/SignIn/SignIn.jsx"; // ChatBot component
 
 const router = createBrowserRouter([
+
+    //Uncomment this to run sign in page
+  {
+    path: "/",
+    element: <Register />,
+  },
+
+
+
+    //Uncomment this to run Home page
   {
     path: "/",
     element: <Home />, // Home page
@@ -17,6 +33,26 @@ const router = createBrowserRouter([
     path: "/chatbot",
     element: <ChatBot />, // ChatBot page
   },
+
+
+//Uncomment this to run Admin panel
+//   {
+//     path: "/",
+//     element: <Dashboard />,
+//   },
+//   {
+//     path: "/add-course",
+//     element: <AddCourse />,
+//   },
+//   {
+//     path: "/user-details",
+//     element: <User />,
+//   },
+//   {
+//     path: "/feedback",
+//     element: <Feedback />,
+//   },
+
 ]);
 
 const ContextProvider = ({ children }) => {
