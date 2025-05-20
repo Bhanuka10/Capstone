df7e3b5350edd6b3d40ef07a1514bf642964d5f2
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-05-20 22:49:42 +0530

    create road map

---
+++ KnowEdge-app/src/Components/Roadmap/Roadmap.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Roadmap.css"
+
+const Roadmap = () => {
+  return (
+    <div>
+        <div className='roadmap'>
+            <h1>gggggggggg</h1>
+
+        </div>
+    </div>
+  )
+}
+
+export default Roadmap
